78c1888c247ec6b4922946e0925ad59da11f70c9
Sebastian Koszuta <sebastian.koszuta@gmail.com>
2017-12-21 14:17:33 +0100

    Set up Jest

---
+++ src/utils/get-node-by-path.test.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import getNodeByPath from './get-node-by-path';
+
+const { graph: tree } = JSON.parse(fs.readFileSync('json/order.json', 'utf8'));
+
+describe('getNodeByPath', () => {
+  it('should return the correct node from the root of the tree', () => {
+    expect(getNodeByPath([5], tree)).toBe('0894c5');
+  });
+
+  it('should return the correct node from the middle of the tree', () => {
+    expect(getNodeByPath([1, 'Purchase Consideration and Behavior', 0], tree)).toEqual({
+      "Taxis": ["ede6a8", "62c00f", "0f6ce0", "f2a681"]
+    });
+  });
+
+  it('should return the correct node from the end of the tree', () => {
+    expect(getNodeByPath([0, 'Awareness Metrics', 0, 'Taxis', 1], tree)).toBe('7a89e0');
+  });
+});
